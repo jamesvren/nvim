b7aaf344b40795bbe86b03377213bb13ee8ccfd2
jamesrern <jamesvren@163.com>
2024-03-20 10:01:34 +0000

    disable more plugins

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,4 +1,7 @@
 return {
-  { "L3MON4D3/LuaSnip", enabled = false},
-  { "lewis6991/gitsigns.nvim", enabled = false},
+  { "L3MON4D3/LuaSnip", enabled = false },
+  -- { "lewis6991/gitsigns.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
 }
